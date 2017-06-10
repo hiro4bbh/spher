cd37b536c08b3fdd0143960c281658d1d0b97c69
Tatsuhiro Aoshima <hiro4bbh@gmail.com>
2017-06-10 20:04:02 +0900

    Implement Round64 and Vector64.Clone

--- float64.go
+++ float64.go
@@ -2,8 +2,33 @@ package spher
 
 import "math"
 
+// Round float64 x to the specified number of decimal places.
+func Round64(x float64, n int) float64 {
+	shift := math.Pow(10, float64(n))
+	return math.Floor(x * shift + 0.5) / shift
+}
+
 // Vector64 is a dense float64 vector.
+// Unless specified, methods return self.
 type Vector64 []float64
+
+// Compares Vector64 x and y.
+// Returns +1 if x is greater than y, -1 if x is smaller than y, otherwise 0.
+func Cmp64(x, y Vector64) int {
+	if len(x) < len(y) {
+		return -1
+	} else if len(x) > len(y) {
+		return +1
+	}
+	for i := 0; i < len(x); i++ {
+		if x[i] < y[i] {
+			return -1
+		} else if x[i] > y[i] {
+			return +1
+		}
+	}
+	return 0
+}
 
 // Returns the dot product of Vector64 x and y.
 func Dot64(x, y Vector64) float64 {
@@ -12,24 +37,45 @@ func Dot64(x, y Vector64) float64 {
 	}
 	s := float64(0)
 	for i := 0; i < len(x); i++ {
-		s += x[i]*y[i]
+		s += x[i] * y[i]
 	}
 	return s
 }
 
 // Returns L2-norm of Vector64 x.
 func L2Norm64(x Vector64) float64 {
+	if len(x) == 0 {
+		return math.NaN()
+	}
 	return math.Pow(Dot64(x, x), 0.5)
 }
 
+// Returns a clone of Vector64 x.
+// A clone is not affected by changes on x, and x is not affected by its clones.
+func (x Vector64) Clone() Vector64 {
+	cloneX := make(Vector64, len(x))
+	copy(cloneX, x)
+	return cloneX
+}
+
 // Multiply Vector64 x by scalar y.
-func (x Vector64) MulS(y float64) {
+func (x Vector64) MulS(y float64) Vector64 {
 	for i := 0; i < len(x); i++ {
 		x[i] *= y
 	}
+	return x
 }
 
 // Normalize Vector64 x.
-func (x Vector64) Normalize() {
-	x.MulS(1.0/L2Norm64(x))
+func (x Vector64) Normalize() Vector64 {
+	x.MulS(1.0 / L2Norm64(x))
+	return x
+}
+
+// Round the elements of Vector64 x to the specified number of decimal places.
+func (x Vector64) Round(n int) Vector64 {
+	for i := 0; i < len(x); i++ {
+		x[i] = Round64(x[i], n)
+	}
+	return x
 }
--- float64_test.go
+++ float64_test.go
@@ -3,27 +3,93 @@ package spher
 import "math"
 import "testing"
 
+const FLOAT64_NORMAL_PRECISION = 12
+
+func eq64Normal(x, y float64) bool {
+	return Round64(x, FLOAT64_NORMAL_PRECISION) == Round64(y, FLOAT64_NORMAL_PRECISION)
+}
+
+func eqVector64Normal(x, y Vector64) bool {
+	return Cmp64(x.Clone().Round(FLOAT64_NORMAL_PRECISION), x.Clone().Round(FLOAT64_NORMAL_PRECISION)) == 0
+}
+
+func TestRound64(t *testing.T) {
+	test := func(expected float64, x float64, n int) {
+		if got := Round64(x, n); expected != got {
+			t.Errorf("Round64(%f, %d): expected %f, but got %f", x, n, expected, got)
+		}
+	}
+	test(1.0, 1.105, 0)
+	test(2.0, 1.605, 0)
+	test(1.1, 1.1105, 1)
+	test(1.2, 1.1605, 1)
+	test(0.0, 1.05, -1)
+	test(10.0, 6.05, -1)
+}
+
+func TestCmp64(t *testing.T) {
+	test := func(expected int, x, y Vector64) {
+		if got := Cmp64(x, y); expected != got {
+			t.Errorf("Cmp64(%#v, %#v): expected %d, but got %d", x, y, expected, got)
+		}
+	}
+	test(0, Vector64{1.0, 2.0, 3.0}, Vector64{1.0, 2.0, 3.0})
+	test(1, Vector64{1.0, 3.0, 3.0}, Vector64{1.0, 2.0, 3.0})
+	test(-1, Vector64{1.0, 1.0, 3.0}, Vector64{1.0, 2.0, 3.0})
+	test(1, Vector64{1.0, 2.0, 3.0}, Vector64{1.0, 2.0})
+	test(-1, Vector64{1.0, 2.0}, Vector64{1.0, 2.0, 3.0})
+}
+
 func TestDot64(t *testing.T) {
-	x1 := Vector64{1.0, 2.0, 3.0, 4.0, 5.0}
-	y1 := Vector64{5.0, 4.0, 3.0, 2.0, 1.0}
-	if expected, got := 35.0, Dot64(x1, y1); expected != got {
-		t.Errorf("Dot64(x1, y1): expected %f, but got %f", expected, got)
+	test := func(expected float64, x, y Vector64) {
+		if got := Dot64(x, y); (math.IsNaN(expected) && !math.IsNaN(got)) || (!math.IsNaN(expected) && !eq64Normal(expected, got)) {
+			t.Errorf("Dot64(%#v, %#v): expected %f, but got %f", x, y, expected, got)
+		}
 	}
-	x2 := Vector64{}
-	y2 := Vector64{}
-	if expected, got := 0.0, Dot64(x2, y2); expected != got {
-		t.Errorf("Dot64(x2, y2): expected %f, but got %f", expected, got)
+	test(10.0, Vector64{1.0, 2.0, 3.0}, Vector64{3.0, 2.0, 1.0})
+	test(0.0, Vector64{}, Vector64{})
+	test(math.NaN(), Vector64{1.0, 2.0, 3.0}, Vector64{})
+}
+
+func TestL2Norm64(t *testing.T) {
+	test := func(expected float64, x Vector64) {
+		if got := L2Norm64(x); (math.IsNaN(expected) && !math.IsNaN(got)) || (!math.IsNaN(expected) && !eq64Normal(expected, got)) {
+			t.Errorf("L2Norm64(%#v): expected %f, but got %f", x, expected, got)
+		}
 	}
-	if got := Dot64(x1, y2); !math.IsNaN(got) {
-		t.Errorf("Dot64(x1, y2): expected NaN, but got %f", got)
+	test(5.0, Vector64{3.0, 4.0})
+	test(math.NaN(), Vector64{})
+}
+
+func TestVector64CloneX(t *testing.T) {
+	x := Vector64{1.0, 2.0, 3.0}
+	cloneX := x.Clone()
+	x[0] = 4.0
+	if cloneX[0] != 1.0 {
+		t.Errorf("%#v.Clone(): A clone of Vector64 must not be affected by changes of the original", x)
+	}
+	cloneX[1] = 5.0
+	if x[1] != 2.0 {
+		t.Errorf("%#v.Clone(): The original Vector64 must not be affected by its clones", x)
 	}
 }
 
-func TestVector64Normalize(t *testing.T) {
-	// Also tests L2Norm64 and Vector64.MulS implicitly.
-	x1 := Vector64{3.0, 4.0}
-	x1.Normalize()
-	if expected, got := 1.0, L2Norm64(x1); expected != got {
-		t.Errorf("L2Norm64(x1.Normalize()): expected %f, but got %f", expected, got)
+func TestVector64MulS(t *testing.T) {
+	test := func(expected Vector64, x Vector64, y float64) {
+		if got := x.Clone(); !eqVector64Normal(expected, got.MulS(y)) {
+			t.Errorf("%#v.MulS(%f): expected %#v, but got %#v", x, y, expected, got)
+		}
 	}
+	test(Vector64{2.0, 4.0, 6.0}, Vector64{1.0, 2.0, 3.0}, 2.0)
+	test(Vector64{}, Vector64{}, 2.0)
+}
+
+func TestVector64Normalize(t *testing.T) {
+	test := func(expected Vector64, x Vector64) {
+		if got := x.Clone(); !eqVector64Normal(expected, got.Normalize()) {
+			t.Errorf("%#v.Normalize(): expected %#v, but got %#v", x, expected, got)
+		}
+	}
+	test(Vector64{3.0/5.0, 4.0/5.0}, Vector64{3.0, 4.0})
+	test(Vector64{}, Vector64{})
 }
