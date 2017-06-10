99ee6ae0b9d1a01241d134c12d5413f8b0ce25a1
Tatsuhiro Aoshima <hiro4bbh@gmail.com>
2017-06-10 17:36:26 +0900

    Implement Vector64.Normalize

--- float64.go
+++ float64.go
@@ -2,27 +2,34 @@ package spher
 
 import "math"
 
-// Returns true if x is NaN64, otherwise returns false.
-func IsNaN64(x float64) bool {
-	return math.IsNaN(x)
-}
-
-// Returns float64 NaN.
-func NaN64() float64 {
-	return math.NaN()
-}
-
 // Vector64 is a dense float64 vector.
 type Vector64 []float64
 
-// Returns the dot product of x and y.
+// Returns the dot product of Vector64 x and y.
 func Dot64(x, y Vector64) float64 {
 	if len(x) != len(y) {
-		return NaN64()
+		return math.NaN()
 	}
 	s := float64(0)
 	for i := 0; i < len(x); i++ {
 		s += x[i]*y[i]
 	}
 	return s
+}
+
+// Returns L2-norm of Vector64 x.
+func L2Norm64(x Vector64) float64 {
+	return math.Pow(Dot64(x, x), 0.5)
+}
+
+// Multiply Vector64 x by scalar y.
+func (x Vector64) MulS(y float64) {
+	for i := 0; i < len(x); i++ {
+		x[i] *= y
+	}
+}
+
+// Normalize Vector64 x.
+func (x Vector64) Normalize() {
+	x.MulS(1.0/L2Norm64(x))
 }
--- float64_test.go
+++ float64_test.go
@@ -1,19 +1,29 @@
 package spher
 
+import "math"
 import "testing"
 
 func TestDot64(t *testing.T) {
 	x1 := Vector64{1.0, 2.0, 3.0, 4.0, 5.0}
 	y1 := Vector64{5.0, 4.0, 3.0, 2.0, 1.0}
 	if expected, got := 35.0, Dot64(x1, y1); expected != got {
-		t.Errorf("Dot(x1, y1): expected %f, but got %f", expected, got)
+		t.Errorf("Dot64(x1, y1): expected %f, but got %f", expected, got)
 	}
 	x2 := Vector64{}
 	y2 := Vector64{}
 	if expected, got := 0.0, Dot64(x2, y2); expected != got {
-		t.Errorf("Dot(x2, y2): expected %f, but got %f", expected, got)
+		t.Errorf("Dot64(x2, y2): expected %f, but got %f", expected, got)
 	}
-	if got := Dot64(x1, y2); !IsNaN64(got) {
-		t.Errorf("Dot(x1, y2): expected NaN64, but got %f", got)
+	if got := Dot64(x1, y2); !math.IsNaN(got) {
+		t.Errorf("Dot64(x1, y2): expected NaN, but got %f", got)
+	}
+}
+
+func TestVector64Normalize(t *testing.T) {
+	// Also tests L2Norm64 and Vector64.MulS implicitly.
+	x1 := Vector64{3.0, 4.0}
+	x1.Normalize()
+	if expected, got := 1.0, L2Norm64(x1); expected != got {
+		t.Errorf("L2Norm64(x1.Normalize()): expected %f, but got %f", expected, got)
 	}
 }
